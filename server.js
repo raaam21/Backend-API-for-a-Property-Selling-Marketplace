515e6907ab77fe10aee07409fad818717ffe59d7
Bramharishi Shukla <bramharishi21@gmail.com>
2024-03-23 23:34:42 +0530

    Patch Route Added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection.js");
-const {addProperty,getProperty, deleteProperty} = require("./controllers/propControllers.js")
+const {addProperty,getProperty, deleteProperty,updateProperty} = require("./controllers/propControllers.js")
 const pModel = require("./models/propertyModel.js");
 const {mongoose } = require("mongoose");
 const expressAsyncHandler = require("express-async-handler");
@@ -18,18 +18,7 @@ app.get('/',getProperty);
 
 app.delete('/:id',deleteProperty);
 
-app.patch('/:id',async(req,res)=>{
-    console.log('Updating property');
-    const updatedProperty = await pModel.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new:true}
-    );
-    res.status(200).json(updatedProperty);
-
-
-    // res.status(200);
-});
+app.patch('/:id',updateProperty);
 
 
 
