93f1e885f023527b8b6b65c80584558db25581fa
Bramharishi Shukla <bramharishi21@gmail.com>
2024-03-29 20:09:21 +0530

    Modifying for Cloud Env

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,9 +1,9 @@
 
 const mongoose = require("mongoose");
-const CONNECTION_STRING = 'mongodb+srv://yoursram18:xYBNAhtNBmjMIbQZ@cluster0.vi9bia2.mongodb.net/'
+
 const connectDb = async()=>{
     try{
-        const connect = await mongoose.connect(CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log(
             "DB connected:",
             connect.connection.host,
